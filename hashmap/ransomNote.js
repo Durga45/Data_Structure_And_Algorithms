ad5153cf57499d160a94089798d52ecc0c9c6f25
Durga45 <dspandey051@gmail.com>
2025-05-10 15:33:46 +0530

    hashtable

---
+++ hashmap/ransomNote.js
@@ -0,0 +1,13 @@
+function canConstruct(ransomNote, magazine) {
+    let magazineCount={}
+    for(let char of magazine){
+       magazineCount[char]=(magazineCount[char]||0)+1
+    }
+    for(let char of ransomNote){
+        if(!magazineCount[char]){
+            return false
+        }
+        magazineCount[char]--
+    }
+    return true
+};
